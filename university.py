e59c81e52e821dd45c9c7d530eda42155f771a56
sasini <sasinidr@gmail.com>
2024-05-18 12:43:35 +0530

    Create module variables

--- university.py
+++ university.py
@@ -1,10 +1,14 @@
 from student import Student
+from module import Module
 
 stu1 = Student("Devi", 200134564500)
 stu2 = Student("Dasu", 199867540987)
 
+module1 = Module("Maths", "IM3245")
+module2 = Module("Programming", "DA3256")
 
 age = 12
 print("Hi, New Student")
 print(age)
 print(stu2.nic)
+print(module1.name)
