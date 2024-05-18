f0d51f1e52efdf3204e6b3c0918458519051ff3b
umesha731 <umeshaidabare@gmail.com>
2024-05-18 19:45:47 +0530

    Update university.py

--- university.py
+++ university.py
@@ -4,9 +4,11 @@ from module import Module
 
 stu1 = Student("Devi", 200134564500)
 stu2 = Student("Dasu", 199867540987)
+stu3 = Student("Dinusha", 200345379871)
 
 module1 = Module("Maths", "IM3245")
 module2 = Module("Programming", "DA3256")
+module3 = Module("Operation Management, "BA4532")
 
 dep1 = Department("Civil")
 dep2 = Department("IT")
